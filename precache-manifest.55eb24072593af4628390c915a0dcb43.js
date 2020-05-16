28d50b373f2f44964636fe49bab37f8402a05931
Ravi Baratha <abaratharaj@palantir.com>
2020-05-16 19:00:23 +0100

    Updates

---
+++ precache-manifest.55eb24072593af4628390c915a0dcb43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491442e69125a3be1ea94c144791ba9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de6a0381ed48d2cab928",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "469eda7beeb132c21fbc",
+    "url": "/static/js/2.293adf50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.293adf50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de6a0381ed48d2cab928",
+    "url": "/static/js/main.24829729.chunk.js"
+  },
+  {
+    "revision": "0d6988e3e4e6800d87b6",
+    "url": "/static/js/runtime-main.5a649683.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
