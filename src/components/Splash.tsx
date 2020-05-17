4a26837b85888250c1dcc28081c549f9005524e5
Ravi Baratha <abaratharaj@palantir.com>
2020-05-17 13:06:06 +0100

    Adding some basic components

---
+++ src/components/Splash.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export class Splash extends React.PureComponent<{}, {}> {
+
+    public render() {
+        return (
+            <div className="splash-container">
+                <span>This is the splash component</span>
+            </div>
+        )
+    }
+}
